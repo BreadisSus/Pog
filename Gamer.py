9f943c24cd57d370a4f8d678bec09276e4294457
BreadisSus <poidfszfdfsdfsdf@protonmail.com>
2021-09-08 22:49:43 +0100

    pythons

--- Gamer.py
+++ Gamer.py
@@ -1,4 +1,16 @@
-wow2="ok"
-wow1="no"
-name =("ok","no")
-print=wow1
+gaming = int(input("wow so cool "))
+somenames = ["gamer", "chad", "chadette"]
+lupes = ("yes", "no")
+print (somenames[1])
+print (gaming)
+print (lupes)
+
+
+if gaming > 20:
+    print ("gaming mroe than 20")
+
+elif gaming < 20:
+    print ("gaming less than 20")
+
+else:
+    print(" is 20")
