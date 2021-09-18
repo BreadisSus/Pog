97a9c319b6ba5451e7970486256373fcb532d12d
BreadisSus <poidfszfdfsdfsdf@protonmail.com>
2021-09-18 15:37:39 +0100

    More of the pythons

---
+++ idk.py
@@ -0,0 +1,15 @@
+
+Dict = {"gamer": "bruh", "thomas": "poggers", "jamie": "stupid poopoo sid from toy story look alike", "supertov": "a made up word that means, gamering in the halls of gaming hoaoohoaoh"}
+
+Addd = (input("want to add something to the dict? "))
+if Addd == "yes":
+    huh = (input("Word "))
+    huhh = (input("Meaning "))
+    Dict[huh] = huhh
+
+elif Addd == "no":
+    print("well then dont you dumb dumb ")
+
+
+gamering = (input("Type something ")) 
+print (Dict[gamering])
